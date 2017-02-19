314057d9d479197e0f31a5dd56bdb125bc0d354a
Niko Häikiö <niko.haikio@gmail.com>
2017-02-19 16:47:44 +0200

    Fixed a bug

--- src/model/mod.cpp
+++ src/model/mod.cpp
@@ -24,7 +24,8 @@ Mod::Mod(const QString& name, const QString& key):
     //Enables non lagging UI
     moveToThread(Global::workerThread);
     qRegisterMetaType<QVector<int>>("QVector<int>");
-    QMetaObject::invokeMethod(this, "threadConstructor", Qt::QueuedConnection);
+    //Blocking is required, because otherwise startUpdates might be called before updateTimer_ is initialzied.
+    QMetaObject::invokeMethod(this, "threadConstructor", Qt::BlockingQueuedConnection);
 }
 
 Mod::~Mod()
