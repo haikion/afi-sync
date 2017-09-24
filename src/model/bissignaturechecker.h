7a114f76d7b57d3b86e2997c0147fad9579eb822
Niko Häikiö <niko.haikio@gmail.com>
2017-09-24 21:44:18 +0300

    Bis signature check

--- src/model/bissignaturechecker.h
+++ src/model/bissignaturechecker.h
@@ -1,11 +1,38 @@
 #ifndef BISSIGNATURECHECKER_H
 #define BISSIGNATURECHECKER_H
 
+#include <QString>
+#include <QSet>
+#include <QObject>
+#include <QQueue>
+#include <QSharedPointer>
+#include "sigcheckprocess.h"
 
-class BisSignatureChecker
+static void doDeleteLater(QObject* obj)
 {
+    obj->deleteLater();
+}
+
+class BisSignatureChecker : public QObject
+{
+    Q_OBJECT
+
 public:
-    BisSignatureChecker();
+    BisSignatureChecker(QObject* parent = nullptr);
+    QSharedPointer<SigCheckProcess> check(const QString& modPath, const QSet<QString>& filePaths);
+    bool isQueued(const QString& modPath) const;
+    bool isChecking(const QString& modPath) const;
+
+signals:
+    void checkDone();
+
+private:
+    QQueue<QSharedPointer<SigCheckProcess>> executionQueue_;
+
+    QSharedPointer<SigCheckProcess> createProcess(const QString& modPath, const QSet<QString>& filePaths) const;
+
+private slots:
+    void processQueue();
 };
 
 #endif // BISSIGNATURECHECKER_H
