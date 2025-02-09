f3ce821c4ad8b130beedeb72f21efaea0c15a90f
Niko Häikiö <niko.haikio@gmail.com>
2025-02-09 22:11:25 +0200

    Adjust status column width so that "Downloading Patch..." is fully visible

--- app/src/view/astreewidget.cpp
+++ app/src/view/astreewidget.cpp
@@ -73,7 +73,7 @@ void AsTreeWidget::setRepositories(const QList<IRepository*>& repositories)
     items_.clear();
     addRepositories(repositories);
     header()->resizeSections(QHeaderView::ResizeToContents);
-    header()->resizeSection(2, header()->sectionSize(2) + 50); //Status
+    header()->resizeSection(2, header()->sectionSize(2) + 80); //Status
     header()->resizeSection(4, header()->sectionSize(4) + 25); //Size
     //Hack to make name column take remaining space
     header()->setSectionResizeMode(1, QHeaderView::Stretch);
